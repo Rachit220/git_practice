593b4568dd462b7c7b2d13c89951af6506dafbca
Rachit220 <160825604+Rachit220@users.noreply.github.com>
2025-12-18 16:22:33 +0530

    Add files via upload
    Personal Info Script

---
+++ difreent.py
@@ -0,0 +1,34 @@
+# Storing personal information using different data types
+
+# String
+name = "Rachit Prajapati"
+
+# Integer
+age = 20
+
+# Float
+height = 5.8
+
+# Boolean
+is_student = True
+
+# List
+skills = ["Python", "Git", "Problem Solving"]
+
+# Dictionary
+address = {
+    "city": "Ahmedabad",
+    "state": "Gujarat",
+    "country": "India"
+}
+
+# Displaying the information
+print("----- Personal Information -----")
+print("Name:", name)
+print("Age:", age)
+print("Height:", height, "feet")
+print("Student:", is_student)
+print("Skills:", skills)
+print("City:", address["city"])
+print("State:", address["state"])
+print("Country:", address["country"])
